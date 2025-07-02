191fcdc77182d8e0c58eba81f6c60840c7bf27d2
Chris Child <996720+03c@users.noreply.github.com>
2025-07-02 13:09:17 +0100

    allow OPTIONS and CORS Origin of *

--- internal/api/api.go
+++ internal/api/api.go
@@ -34,6 +34,20 @@ func NewAPI(cfg *config.Config, mailcowClient *mailcow.MailcowClient, authModule
 		router:        mux.NewRouter(),
 		logger:        logger.WithComponent("API"),
 	}
+
+	api.router.Use(func(next http.Handler) http.Handler {
+                return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                        w.Header().Set("Access-Control-Allow-Origin", "*")
+                        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+                        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authentication")
+                        if r.Method == http.MethodOptions {
+                                w.WriteHeader(http.StatusOK)
+                                return
+                        }
+                        next.ServeHTTP(w, r)
+                })
+        })
+	
 	api.routes()
 	return api
 }
@@ -44,6 +58,9 @@ func (a *API) Router() http.Handler {
 }
 
 func (a *API) routes() {
+	a.router.PathPrefix("/").Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                w.WriteHeader(http.StatusOK)
+        })
 	a.router.HandleFunc("/api/alias/random/new", a.handleNewAlias).Methods("POST")
 	a.logger.Debug("Registered route: POST /api/alias/random/new")
 }
